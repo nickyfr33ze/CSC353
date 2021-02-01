bfa61778dcfc500761276dddabb76ae4d0b67e6f
Nick <43529027+nickyfr33ze@users.noreply.github.com>
2021-02-01 10:00:50 -0700

    Disabled Mass Assignment for everything except id()

--- helloWorld/app/Models/Lecture.php
+++ helloWorld/app/Models/Lecture.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Lecture extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id']; //This disables the built in protection for all of the table EXCEPT for id() as we do not this table edited ; there is another option to list out every class to disable but it is more tedious
+
+    $newLecture = Lecture::create([
+       'speaker_name' => 'Nick',
+       'title' => 'Getting started with Laravel'
+       'outline' => 'This is my outline '
+       //this is all an example of MassAssignment
+    ])
 }
--- helloWorld/database/migrations/2021_02_01_163207_create_lectures_table.php
+++ helloWorld/database/migrations/2021_02_01_163207_create_lectures_table.php
@@ -15,10 +15,10 @@ class CreateLecturesTable extends Migration
     {
         Schema::create('lectures', function (Blueprint $table) { //Passes in a closure known as 'Blueprint'
             $table->id();   //This is where you can find all of laravel's available types 'https://laravel.com/docs/8.x/migrations#available-column-types'
-            $table->string('speaker_name');     //Limited to 260 characters
+            $table->string('speaker_name');     //Limited to 260 characters ; translated to a varchar character type ; has ability to be indexed and optimized in search type queries in MySQL
             $table->string('title');    //Limited to 260 characters ; these are used to save resources in memory
             $table->text('outline');    //Limited to 2GB of string data
-            $table->timestamps();
+            $table->timestamps();   //These types all depend on what exactly you are storing in your DB ; a name would do fine as a string type ; a lecture may be better as text 
         });
     }
 
