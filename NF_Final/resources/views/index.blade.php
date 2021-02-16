3ddcc02362b67d55c9a9ddc7fac1d971e681a929
Nick <43529027+nickyfr33ze@users.noreply.github.com>
2021-02-16 16:27:32 -0700

    Updated code

--- NF_Final/resources/views/index.blade.php
+++ NF_Final/resources/views/index.blade.php
@@ -2,6 +2,6 @@
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
-<html style="background-color: ">
+<html style="background-color: darkolivegreen">
 
 </html>
