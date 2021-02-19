b645cc4300ed6a78165dfdc18b67ed1cbfa5a655
Nick <43529027+nickyfr33ze@users.noreply.github.com>
2021-02-19 10:40:58 -0700

    Created a talk model to assist in directing

---
+++ NF_Final/database/seeders/TalkSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TalkSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
