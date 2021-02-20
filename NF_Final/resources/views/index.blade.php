8590cb8ccddc029a0a7cbe272d0100ac9c22399c
Nick <43529027+nickyfr33ze@users.noreply.github.com>
2021-02-20 13:26:22 -0700

    Edited some styling colors and added links

--- NF_Final/resources/views/index.blade.php
+++ NF_Final/resources/views/index.blade.php
@@ -1,21 +1,25 @@
 <!DOCTYPE html>
+<!-- We will utilize the Tailwind CSS and maybe some things from alpine.js -->
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
-<html style="background-color: darkolivegreen">
+<html>
     <body>
         <center>
-        <div class="text-3xl text-black font-mono">
-            Welcome to Nick's web page
+        <div class="text-2xl text-black font-mono" style="background-color: darkolivegreen">
+            Welcome to Nick's live web page
             <br>
             <h1>
                 Please click one of the following links to explore the features!
             </h1>
+            <div>
+                <a href="/documents">
+                <a href="/images">
+                <a href="/misc">
+            </div>
         </div>
         </center>
         <!-- This is where the code for the buttons/links -->
-         <a href="">
-         <a href="">
-         <a href="">
+         
     </body>
 </html>
