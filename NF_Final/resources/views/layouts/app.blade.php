3146f59a681eaf08cdbc6720c05158c47428a24a
Nick <43529027+nickyfr33ze@users.noreply.github.com>
2021-02-24 10:03:24 -0700

    Fixed syntax typo

--- NF_Final/resources/views/layouts/app.blade.php
+++ NF_Final/resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
             <!-- Page Heading -->
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header ?? 'blah in app.blade.php }}
+                    {{ $header ?? 'blah in app.blade.php' }}
                 </div>
             </header>
 
