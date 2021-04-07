d6a57cf8768785a15f2d4612b2d8dc6c4b1583dd
Nick Friesen <nfriesen26@gmail.com>
2021-04-06 21:53:01 -0600

    update

--- FINAL_SITE/resources/views/index.blade.php
+++ FINAL_SITE/resources/views/index.blade.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
+
 <html>
     <title>Index</title>
-    <body>
-        <p>This is the index page using Tailwind (maybe alpine idk yet)
+    <body class="">
+        <center>
+        <p>This is the index page using Tailwind (maybe alpine idk yet)</p>
+        </center>
     </body>
 </html>
